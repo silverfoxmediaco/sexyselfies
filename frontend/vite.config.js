575c5694dc0b92490a6596b2432e2740e309c71b
James-mcewen <james@silverfoxmedia.co>
2025-08-24 20:09:52 -0500

    fixing json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+          emotion: ['@emotion/react', '@emotion/styled']
+        }
+      }
+    }
+  },
   server: {
     port: 5174,
     proxy: {
